2aab211ece3200add6d45cad536dd0cf82e7e969
parroty <parroty00@gmail.com>
2012-07-20 00:52:58 +0900

    refactored class structure

--- lib/kindle-highlights.rb
+++ lib/kindle-highlights.rb
@@ -129,17 +129,32 @@ class KindleHighlight::Highlight
   end
 end
 
-class KindleHighlight::XML
-  def self.convert(list)
+
+class KindleHighlight::BaseFormat
+  def initialize(options)
+    @file_name   = options[:file_name] || "output"
+    @output_path = options[:output_path] || "."
+    @list        = options[:list] || []
+  end
+
+  def save(str)
+    File.open(@file_name, "w") do | f |
+      f.puts str
+    end
+  end
+end
+
+class KindleHighlight::XML < KindleHighlight::BaseFormat
+  def output
     builder = Nokogiri::XML::Builder.new do | xml |
       xml.books {
-        list.books.each do | b |
+        @list.books.each do | b |
           xml.book {
             xml.asin b.asin
             xml.title b.title
             xml.author b.author
 
-            list.highlights_hash[b.asin].each do | h |
+            @list.highlights_hash[b.asin].each do | h |
               xml.highlights {
                 xml.annotation_id h.annotation_id
                 xml.content h.content
@@ -149,14 +164,27 @@ class KindleHighlight::XML
         end
       }
     end
-    builder.to_xml
+
+    save(builder.to_xml)
   end
 end
 
-class KindleHighlight::HTML
-  def self.convert(list)
+class KindleHighlight::HTML < KindleHighlight::BaseFormat
+  def output
+    output_html
+    copy_styles
+  end
+
+private
+  def output_html
     file_name = File.dirname(__FILE__) + "/template/kindle.html.erb"
-    namespace = OpenStruct.new(:books => list.books, :highlights => list.highlights_hash)
+    namespace = OpenStruct.new(:books => @list.books, :highlights => @list.highlights_hash)
     template = ERB.new(File.read(file_name)).result(namespace.instance_eval { binding })
+    save(template)
+  end
+
+  def copy_styles
+    src = File.dirname(__FILE__) + "/template/bootstrap"
+    FileUtils.cp_r(src, @output_path)
   end
 end
