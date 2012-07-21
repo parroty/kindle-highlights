9393c57a59685a84552ab6317e2c600a37b9a24d
parroty <parroty00@gmail.com>
2012-07-21 12:01:26 +0900

    separated format class into another file

--- lib/kindle-highlights.rb
+++ lib/kindle-highlights.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'mechanize'
 require 'nokogiri'
 require 'erb'
-require 'ostruct'
+require 'kindle-highlights/kindle_format'
 
 class KindleHighlight
   attr_accessor :highlights, :books
@@ -130,65 +130,5 @@ class KindleHighlight::Highlight
     book = KindleHighlight::Book.find(self.asin)
     self.author = book[:author]
     self.title = book[:title]
-  end
-end
-
-
-class KindleHighlight::BaseFormat
-  def initialize(options)
-    @file_name   = options[:file_name] || "output"
-    @output_path = options[:output_path] || "."
-    @list        = options[:list] || []
-  end
-
-  def save(str)
-    File.open([@output_path, @file_name].join("/"), "w") do | f |
-      f.puts str
-    end
-  end
-end
-
-class KindleHighlight::XML < KindleHighlight::BaseFormat
-  def output
-    builder = Nokogiri::XML::Builder.new do | xml |
-      xml.books {
-        @list.books.each do | b |
-          xml.book {
-            xml.asin b.asin
-            xml.title b.title
-            xml.author b.author
-
-            @list.highlights_hash[b.asin].each do | h |
-              xml.highlights {
-                xml.annotation_id h.annotation_id
-                xml.content h.content
-              }
-            end
-          }
-        end
-      }
-    end
-
-    save(builder.to_xml)
-  end
-end
-
-class KindleHighlight::HTML < KindleHighlight::BaseFormat
-  def output
-    output_html
-    copy_styles
-  end
-
-private
-  def output_html
-    file_name = File.dirname(__FILE__) + "/template/kindle.html.erb"
-    namespace = OpenStruct.new(:books => @list.books, :highlights => @list.highlights_hash)
-    template = ERB.new(File.read(file_name)).result(namespace.instance_eval { binding })
-    save(template)
-  end
-
-  def copy_styles
-    src = File.dirname(__FILE__) + "/template/bootstrap"
-    FileUtils.cp_r(src, @output_path)
   end
 end
